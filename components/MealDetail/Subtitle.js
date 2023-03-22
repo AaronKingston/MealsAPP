abc0ed3594f8cd09b4e507bf823531b737dc661c
AaronKingston <beka.sokurashvili@gmail.com>
2023-03-23 01:32:20 +0400

    Updated meal details screen

---
+++ components/MealDetail/Subtitle.js
@@ -0,0 +1,28 @@
+import { View, Text, StyleSheet } from "react-native";
+
+function Subtitle({ children }) {
+  return (
+    <View style={styles.subtitleContainer}>
+      <Text style={styles.subtitle}>{children}</Text>
+    </View>
+  );
+}
+
+export default Subtitle;
+
+const styles = StyleSheet.create({
+  subtitle: {
+    color: "#e2b497",
+    fontSize: 18,
+    fontWeight: "bold",
+
+    textAlign: "center",
+  },
+  subtitleContainer: {
+    borderBottomColor: "#e2b497",
+    borderBottomWidth: 2,
+    padding: 6,
+    marginHorizontal: 24,
+    marginVertical: 4,
+  },
+});
--- components/MealItem.js
+++ components/MealItem.js
@@ -8,6 +8,7 @@ import {
 } from "react-native";
 
 import { useNavigation } from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
 function MealItem({
   id,
@@ -37,11 +38,11 @@ function MealItem({
             <Image source={{ uri: imageUrl }} style={styles.image} />
             <Text style={styles.title}>{title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration}m</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-          </View>
+          <MealDetails
+            duration={duration}
+            complexity={complexity}
+            affordability={affordability}
+          ></MealDetails>
         </View>
       </Pressable>
     </View>
@@ -78,15 +79,5 @@ const styles = StyleSheet.create({
     textAlign: "center",
     fontSize: 18,
     margin: 8,
-  },
-  details: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    padding: 8,
-  },
-  detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12,
   },
 });
